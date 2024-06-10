d0ad9aef095a97e9b88792e53446d1bacce897be
ShreedattPandya <shreepandyaa12@gmail.com>
2024-06-10 12:22:06 +0530

    About file structure & components, create react element

--- 01basicsreact/src/index.js
+++ 01basicsreact/src/index.js
@@ -39,7 +39,7 @@ root.render(
     <>
     <App />
     <InbuiltComponent />
-    {anotherElement}
-    {reactElement}
+    {anotherElement} //Elements/Variable passed here
+    {reactElement} 
     </>
 );
